1d58ea1794d0c92d2ec7d1aca7d60a8345914ac6
PeyaraFul <arakash022@gmail.com>
2026-05-02 10:38:51 +0600

    env and data fething has been deleted for testing deployment

--- src/lib/Data.jsx
+++ src/lib/Data.jsx
@@ -1,11 +1,5 @@
 export const productsData = async () => { 
- const res = await fetch('https://a8-summer-essential-store-47ltzk65x-peyarafuls-projects.vercel.app/products.json');
+ const res = await fetch('http://localhost:3000/products.json');
 const products = await res.json();
 return products 
-
 }
-    
-
-
-
-
