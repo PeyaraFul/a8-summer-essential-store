0755cf4045f2f895f65114fae2d1f096d7150440
PeyaraFul <arakash022@gmail.com>
2026-05-02 10:11:53 +0600

    rechaged the domein named

--- src/lib/Data.jsx
+++ src/lib/Data.jsx
@@ -1,5 +1,5 @@
 export const productsData = async () => { 
- const res = await fetch('https://a8-summer-essential-store-ebtzi3ts4-peyarafuls-projects.vercel.app//products.json');
+ const res = await fetch('https://a8-summer-essential-store-47ltzk65x-peyarafuls-projects.vercel.app/products.json');
 const products = await res.json();
 return products 
 
--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,7 +1,7 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: "https://a8-summer-essential-store-ebtzi3ts4-peyarafuls-projects.vercel.app/"
+    baseURL: process.env.NEXT_PUBLIC_BASE_URL
 })
 
 
